0aaf324e2d950364036d56eeb4234c3929c65f01
Parth <parth@Parth-Gajjars-Mac-mini.local>
2021-03-16 15:35:57 +0530

    Added Laravel-Mediable in blog

---
+++ app/Models/Media.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Plank\Mediable\Media as MediableModelClass;
+use Illuminate\Support\Facades\Storage;
+
+class Media extends MediableModelClass
+{
+}
